a10300864b81d7405865d564b491c0967965079e
MohammadAbbasi76 <work.abbasii@gmail.com>
2023-11-11 15:35:10 +0330

    bomp

--- components/SpotifyAuthorization/include/JsonExtraction.h
+++ components/SpotifyAuthorization/include/JsonExtraction.h
@@ -8,7 +8,6 @@ extern "C" {
 
 /**
  * @brief This function extracts specific parameters from a JSON string and assigns them to corresponding fields in a TokenParam structure.
- *
  * @param[in] Json The input JSON string.
  * @param[in] SizeJson The size of the JSON string.
  * @return This function does not return a value.
@@ -17,7 +16,6 @@ void ExtractionJsonParamForFindAccessToken(char *Json, size_t SizeJson);
 
 /**
  * @brief This function extracts specific parameters from a JSON string and assigns them to corresponding fields in a UserInfo structure.
- *
  * @param[in] JsonUSerInfo The input JSON string containing user information.
  * @return Returns 0 if the JSON is parsed successfully, or 1 otherwise.
  */

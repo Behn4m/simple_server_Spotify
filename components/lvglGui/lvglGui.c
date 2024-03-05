b2715f3d3fbb88e70c90e831944ef2e569049680
MohammadAbbasi76 <work.abbasii@gmail.com>
2024-03-05 12:05:27 +0330

    save my change

--- components/lvglGui/lvglGui.c
+++ components/lvglGui/lvglGui.c
@@ -5,12 +5,8 @@
 #include "esp_pm.h"
 #include "iot_button.h"
 #include "lvgl__lvgl/src/core/lv_obj.h"
-#define LV_USE_GIF
-#include"lvgl__lvgl/examples/libs/lv_example_libs.h"
-#include"lvgl__lvgl/src/extra/libs/gif/lv_gif.h"
-#include"lvgl__lvgl/src/extra/libs/gif/gifdec.h"
-static void SpotifyPage(void);
 
+static void SpotifyPage(void);
 
 #define BUTTON_UP_GPIO GPIO_NUM_21
 #define BOOT_BUTTON_NUM 0
@@ -72,7 +68,6 @@ static void bar_event_cb(lv_event_t *e)
     lv_obj_t *cont = lv_event_get_target(e);
     int32_t percentOfFillingBar = 0;
     percentOfFillingBar = *((int32_t *)e->param);
-
     if (percentOfFillingBar <= 100)
     {
         lv_bar_set_value(cont, i, LV_ANIM_OFF);
@@ -174,21 +169,6 @@ void MainMenu(void)
     lv_obj_center(spotifyLabel);
 }
 
-void GifTest(void)
-{
-     LV_IMG_DECLARE(img_bulb_gif);
-    lv_obj_t * img;
-
-    img = lv_gif_create(lv_scr_act());
-    lv_gif_set_src(img, &img_bulb_gif);
-    lv_obj_align(img, LV_ALIGN_LEFT_MID, 20, 0);
-
-    img = lv_gif_create(lv_scr_act());
-    /* Assuming a File system is attached to letter 'A'
-     * E.g. set LV_USE_FS_STDIO 'A' in lv_conf.h */
-    lv_gif_set_src(img, "A:lvgl/examples/libs/gif/bulb.gif");
-    lv_obj_align(img, LV_ALIGN_RIGHT_MID, -20, 0);
-}
 /**
  * @brief Function to create the main LVGL user interface
  */
@@ -259,7 +239,73 @@ void LVGL_Timer()
         }
     }
 }
+static void backButtom(lv_event_t *e)
+{
+    lv_obj_t *obj = lv_event_get_target(e);
+    lv_obj_t *menu = lv_event_get_user_data(e);
+    if (lv_menu_back_button_is_root(menu, obj))
+    {
+        lv_obj_t *mbox1 = lv_msgbox_create(NULL);
+        lv_msgbox_add_title(mbox1, "Hello");
+        lv_msgbox_add_close_button(mbox1);
+    }
+}
+void myMenu(void)
+{
+    /*Create a menu object*/
+    lv_obj_t *menu = lv_menu_create(lv_scr_act());
+    lv_obj_set_size(menu, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
+    lv_obj_center(menu);
 
+    /*Modify the header*/
+    lv_obj_t *back_btn = lv_menu_get_main_header_back_btn(menu);
+    lv_obj_t *back_btn_label = lv_label_create(back_btn);
+    lv_obj_add_event_cb(back_btn, backButtom, LV_EVENT_CLICKED, back_btn);
+
+    lv_label_set_text(back_btn_label, "Back");
+
+    lv_obj_t *cont;
+    lv_obj_t *label;
+
+    /*Create sub pages*/
+    lv_obj_t *sub_1_page = lv_menu_page_create(menu, "Page 1");
+
+    cont = lv_menu_cont_create(sub_1_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Hello, I am hiding here");
+
+    lv_obj_t *sub_2_page = lv_menu_page_create(menu, "Page 2");
+
+    cont = lv_menu_cont_create(sub_2_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Hello, I am hiding here");
+
+    lv_obj_t *sub_3_page = lv_menu_page_create(menu, "Page 3");
+
+    cont = lv_menu_cont_create(sub_3_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Hello, I am hiding here");
+
+    /*Create a main page*/
+    lv_obj_t *main_page = lv_menu_page_create(menu, NULL);
+
+    cont = lv_menu_cont_create(main_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Item 1 (Click me!)");
+    lv_menu_set_load_page_event(menu, cont, sub_1_page);
+
+    cont = lv_menu_cont_create(main_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Item 2 (Click me!)");
+    lv_menu_set_load_page_event(menu, cont, sub_2_page);
+
+    cont = lv_menu_cont_create(main_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Item 3 (Click me!)");
+    lv_menu_set_load_page_event(menu, cont, sub_3_page);
+
+    lv_menu_set_page(menu, main_page);
+}
 /**
  * @brief main LVGL gui TASK
  */
@@ -296,7 +342,7 @@ static void LVGL_mainTask(void *pvParameter)
     // Start LVGL timer and create UI
     LVGL_Timer();
     // MainMenu();
-    GifTest();
+    myMenu();
     // LV_UI2();
     // LV_UI3();
     // RailBar();

ea3fe9348b8018567e762700611199e9ce77c7aa
MohammadAbbasi76 <work.abbasii@gmail.com>
2024-03-06 13:00:16 +0330

    code had sorted

--- components/lvglGui/lvglGui.c
+++ components/lvglGui/lvglGui.c
@@ -252,6 +252,9 @@ static void backButtom(lv_event_t *e)
 }
 void myMenu(void)
 {
+    lv_obj_t *cont;
+    lv_obj_t *label;
+
     /*Create a menu object*/
     lv_obj_t *menu = lv_menu_create(lv_scr_act());
     lv_obj_set_size(menu, lv_disp_get_hor_res(NULL), lv_disp_get_ver_res(NULL));
@@ -261,50 +264,39 @@ void myMenu(void)
     lv_obj_t *back_btn = lv_menu_get_main_header_back_btn(menu);
     lv_obj_t *back_btn_label = lv_label_create(back_btn);
     lv_obj_add_event_cb(back_btn, backButtom, LV_EVENT_CLICKED, back_btn);
-
     lv_label_set_text(back_btn_label, "Back");
-
-    lv_obj_t *cont;
-    lv_obj_t *label;
-
-    /*Create sub pages*/
-    lv_obj_t *sub_1_page = lv_menu_page_create(menu, "Page 1");
-
-    cont = lv_menu_cont_create(sub_1_page);
-    label = lv_label_create(cont);
-    lv_label_set_text(label, "Hello, I am hiding here");
-
-    lv_obj_t *sub_2_page = lv_menu_page_create(menu, "Page 2");
-
-    cont = lv_menu_cont_create(sub_2_page);
-    label = lv_label_create(cont);
-    lv_label_set_text(label, "Hello, I am hiding here");
-
-    lv_obj_t *sub_3_page = lv_menu_page_create(menu, "Page 3");
-
-    cont = lv_menu_cont_create(sub_3_page);
-    label = lv_label_create(cont);
-    lv_label_set_text(label, "Hello, I am hiding here");
 
     /*Create a main page*/
     lv_obj_t *main_page = lv_menu_page_create(menu, NULL);
+    lv_menu_set_page(menu, main_page);
 
+    /*Create sub pages*/
+    lv_obj_t *sub_1_page = lv_menu_page_create(menu, "Page 1");
+    cont = lv_menu_cont_create(sub_1_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Hello, I am hiding here");
     cont = lv_menu_cont_create(main_page);
     label = lv_label_create(cont);
     lv_label_set_text(label, "Item 1 (Click me!)");
     lv_menu_set_load_page_event(menu, cont, sub_1_page);
 
+    lv_obj_t *sub_2_page = lv_menu_page_create(menu, "Page 2");
+    cont = lv_menu_cont_create(sub_2_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Hello, I am hiding here");
     cont = lv_menu_cont_create(main_page);
     label = lv_label_create(cont);
     lv_label_set_text(label, "Item 2 (Click me!)");
     lv_menu_set_load_page_event(menu, cont, sub_2_page);
 
+    lv_obj_t *sub_3_page = lv_menu_page_create(menu, "Page 3");
+    cont = lv_menu_cont_create(sub_3_page);
+    label = lv_label_create(cont);
+    lv_label_set_text(label, "Hello, I am hiding here");
     cont = lv_menu_cont_create(main_page);
     label = lv_label_create(cont);
     lv_label_set_text(label, "Item 3 (Click me!)");
     lv_menu_set_load_page_event(menu, cont, sub_3_page);
-
-    lv_menu_set_page(menu, main_page);
 }
 /**
  * @brief main LVGL gui TASK

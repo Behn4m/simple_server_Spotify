34c1e035f60e37ec2deddf8b843e3d588c14349f
M4 <m4ghaniofficial@gmail.com>
2024-07-02 11:51:25 +0330

    add spotify commands

--- main/main.c
+++ main/main.c
@@ -3,7 +3,7 @@
 #include "nvsFlash.h"
 #include "WiFiConfig.h"
 #include "authorization.h"
-//#include "SpotifyInterface.h"
+#include "SpotifyInterface.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #define TIMER_TIME pdMS_TO_TICKS(500) // in millis
@@ -11,26 +11,27 @@
 // ****************************** GLobal Variables ****************************** //
 static const char *TAG = "Main";
 ServiceInterfaceHandler_t InterfaceHandler;
-//SpotifyInterfaceHandler_t SpotifyInterfaceHandler;
+SpotifyInterfaceHandler_t SpotifyInterfaceHandler;
 // ****************************** GLobal Functions ****************************** //
 /**
  * @brief Function to change colors based on a timer callback
  */
-// void SpotifyPeriodicTimer(TimerHandle_t xTimer)
-// {
-//     bool CommandResult = Spotify_SendCommand(SpotifyInterfaceHandler, GetNowPlaying);
-//     if (CommandResult == false)
-//     {
-//         ESP_LOGE(TAG, "Playback info update failed");
-//         return;
-//     }
-//     GUI_UpdateSpotifyScreen(SpotifyInterfaceHandler.PlaybackInfo->ArtistName,
-//                             SpotifyInterfaceHandler.PlaybackInfo->SongName,
-//                             SpotifyInterfaceHandler.PlaybackInfo->AlbumName,
-//                             SpotifyInterfaceHandler.PlaybackInfo->Duration,
-//                             SpotifyInterfaceHandler.PlaybackInfo->Progress);
-//     ESP_LOGI(TAG, "Playback info updated");
-// }
+void SpotifyPeriodicTimer(TimerHandle_t xTimer)
+{
+    bool CommandResult = Spotify_SendCommand(SpotifyInterfaceHandler, GetNowPlaying);
+    if (CommandResult == false)
+    {
+        ESP_LOGE(TAG, "Playback info update failed");
+        return;
+    }
+    GUI_UpdateSpotifyScreen(SpotifyInterfaceHandler.PlaybackInfo->ArtistName,
+                            SpotifyInterfaceHandler.PlaybackInfo->SongName,
+                            SpotifyInterfaceHandler.PlaybackInfo->AlbumName,
+                            SpotifyInterfaceHandler.PlaybackInfo->Duration,
+                            SpotifyInterfaceHandler.PlaybackInfo->Progress);
+    ESP_LOGI(TAG, "Playback info updated");
+}
+
 void app_main(void)
 {
     //GUI_TaskInit();
@@ -50,26 +51,26 @@ void app_main(void)
     InterfaceHandler.ConfigAddressInSpiffs = SpotifyConfigAddressInSpiffs;
     Oauth_TaskInit(&InterfaceHandler);
 
-    // after this semaphore you can use playback command function in every where !
-    // if (xSemaphoreTake(IsSpotifyAuthorizedSemaphore, portMAX_DELAY) == pdTRUE)
-    // {
-    //     bool CommandResult = false;
-    //     CommandResult = Spotify_SendCommand(SpotifyInterfaceHandler, GetUserInfo);
-    //     if (CommandResult == false)
-    //     {
-    //         ESP_LOGE(TAG, "User info update failed");
-    //         return;
-    //     }
-    //     ESP_LOGI(TAG, "User info updated");
-    //     TimerHandle_t xTimer = xTimerCreate("update", TIMER_TIME, pdTRUE, NULL, SpotifyPeriodicTimer);
-    //     xTimerStart(xTimer, 0);
-    //     if (xTimer != NULL)
-    //     {
-    //         if (xTimerStart(xTimer, 0) == pdPASS)
-    //         {
-    //             ESP_LOGI(TAG, "Timer getting start");
-    //         }
-    //     }
-    // }
+    //after this semaphore you can use playback command function in every where !
+    if (xSemaphoreTake(IsSpotifyAuthorizedSemaphore, portMAX_DELAY) == pdTRUE)
+    {
+        bool CommandResult = false;
+        CommandResult = Spotify_SendCommand(SpotifyInterfaceHandler, GetUserInfo);
+        if (CommandResult == false)
+        {
+            ESP_LOGE(TAG, "User info update failed");
+            return;
+        }
+        ESP_LOGI(TAG, "User info updated");
+        TimerHandle_t xTimer = xTimerCreate("update", TIMER_TIME, pdTRUE, NULL, SpotifyPeriodicTimer);
+        xTimerStart(xTimer, 0);
+        if (xTimer != NULL)
+        {
+            if (xTimerStart(xTimer, 0) == pdPASS)
+            {
+                ESP_LOGI(TAG, "Timer getting start");
+            }
+        }
+    }
 #endif
 }
